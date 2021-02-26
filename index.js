9cdbfa1d2a02f0b5feaadee6b90c5282dd9c902d
prahal <vprahaladhan@gmail.com>
2021-02-26 12:33:15 +1100

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {}
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => { 
+  let newEmp = {...employee};
+  newEmp[key] = value;
+  return newEmp;
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => { 
+  employee[key] = value;
+  return employee;
+}
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  let newEmp = {...employee};
+  delete newEmp[key];
+  return newEmp;
+} 
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+}
